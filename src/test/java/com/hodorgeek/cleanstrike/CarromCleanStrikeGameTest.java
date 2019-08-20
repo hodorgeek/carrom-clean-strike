13ee463dcd7d9c1d8d9f73bc23b599a49b10419e
hodorgeek <shambhand2020@gmail.com>
2019-08-20 19:58:09 +0530

    Initial-commit: WIP: setup new gradle project

---
+++ src/test/java/com/hodorgeek/cleanstrike/CarromCleanStrikeGameTest.java
@@ -0,0 +1,13 @@
+package com.hodorgeek.cleanstrike;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CarromCleanStrikeGameTest {
+
+    @Test
+    void testSomething() {
+        assertEquals(true, true);
+    }
+}
